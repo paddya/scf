1ea28f5b91b7be119f94dd61b93bc8139f83a166
paddya <patrick.bisenius@gmail.com>
2014-01-05 11:32:07 +0100

    Init project

---
+++ src/scf/client/Client.java
@@ -0,0 +1,14 @@
+package scf.client;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author paddya
+ */
+public class Client {
+    
+}
