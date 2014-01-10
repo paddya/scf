ab1be2dcacd52d6236d13ef16587f3f9354c746f
paddya <patrick.bisenius@gmail.com>
2014-01-10 20:33:36 +0100

    Client now emulates telnet.

--- src/scf/client/Client.java
+++ src/scf/client/Client.java
@@ -1,9 +1,57 @@
 package scf.client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+
 /**
  *
  * @author paddya
  */
-public class Client {
-    
+public class Client
+{
+
+    public static void main(String[] args) throws IOException
+    {
+
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
+        //System.out.print("Server IP: ");
+
+        String ip = "127.0.0.1"; //scanner.next();
+
+        System.out.print("Port: ");
+
+        Integer port = scanner.nextInt();
+
+        System.out.println(String.format("Connecting to %s on port %d.", ip, port));
+
+        Socket clientSocket = new Socket(ip, port);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        boolean quitFlag = false;
+        
+        while (!quitFlag) {
+            if (scanner.hasNext()) {
+                
+                String sentence = scanner.next();
+                outToServer.writeBytes(sentence + "\n");
+                //String modifiedSentence = inFromServer.readLine();
+                //System.out.println("FROM SERVER: " + modifiedSentence);
+            }
+
+        }
+        
+        clientSocket.close();
+
+    }
 }
+
+
